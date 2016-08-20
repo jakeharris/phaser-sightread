f723baf3d7ac78539e55f9aa95a5a66bc0e13c88
Jake Harris <milieu@javakat.io>
2016-08-19 23:19:49 -0500

    Make basic functions more readable, improve abstraction

--- assets/scripts/states/main.js
+++ assets/scripts/states/main.js
@@ -3,37 +3,37 @@ class Main {
     this.game = game
     this.player = null
     this.background = null
+    this.platforms = []
   }
-  preload() {
-    game.load.spritesheet('robot-unicorn', 'assets/sprites/robot-unicorn.png', 206, 110)
-    game.load.spritesheet('platforms', 'assets/sprites/tiles.png', 16, 16)
-    game.load.image('background', 'assets/sprites/background.png')
-  }
-  create() {
+  
+  configureGame() {
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
-
-    this.game.time.desiredFps = 60
-
-    this.background = this.game.add.tileSprite(0, 0, 800, 600, 'background')
+    this.game.time.desiredFps = 35
+    this.background = this.game.add.tileSprite(0, 0, 1024, 640, 'background')
 
     //this.game.physics.arcade.gravity.y = 250
 
-    this.player = this.game.add.sprite(206, 110, 'robot-unicorn')
-    this.game.physics.enable(this.player, Phaser.Physics.ARCADE)
-
-    this.player.body.bounce.y = 0
-    this.player.body.collideWorldBounds = true
-    this.player.body.setSize(206, 110, 5, 16)
-
-    var runningIndices = []
-    for (var i = 29; i >= 0; i--)
-      runningIndices.push(i)
-    
-    this.player.animations.add('running', runningIndices, 30, true)
-
     this.cursors = this.game.input.keyboard.createCursorKeys()
     this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
-    this.player.animations.play('running')
   }
-  update() {}
+  
+  createStartingPlatforms() {
+    this.platforms.push(new Platform(game, {x: -1, y: 30 }, {width: 60, height: 1}))
+  }
+  
+  
+  preload() {
+    this.game.load.spritesheet('robot-unicorn', 'assets/sprites/robot-unicorn.png', 206, 110)
+    this.game.load.spritesheet('platforms', 'assets/sprites/tiles.png', 16, 16)
+    this.game.load.image('background', 'assets/sprites/background.png')
+  }
+  create() {
+    this.configureGame()
+    this.createStartingPlatforms()
+    
+    this.player = new Player(game)
+  }
+  update() {
+    
+  }
 }
